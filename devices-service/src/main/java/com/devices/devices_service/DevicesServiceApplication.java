7f4b86e9ded6af3563ab66de2c3107c92ab6b073
ckeltoum <chemseddine.keltoum@init.pt>
2026-03-12 12:47:55 +0000

    initialize spring boot service with postgres docker setup

---
+++ devices-service/src/main/java/com/devices/devices_service/DevicesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devices.devices_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevicesServiceApplication.class, args);
+	}
+
+}
---
+++ devices-service/src/test/java/com/devices/devices_service/DevicesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devices.devices_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
